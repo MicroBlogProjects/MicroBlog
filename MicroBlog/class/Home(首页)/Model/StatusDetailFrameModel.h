9c42fae0f979e7c294f887058a391b6ecd73668b
赖伟煌 <laiweihuang126@qq.com>
2015-07-10 12:48:09 +0800

    123

---
+++ MicroBlog/class/Home(首页)/Model/StatusDetailFrameModel.h
@@ -0,0 +1,13 @@
+//
+//  StatusDetailFrameModel.h
+//  MicroBlog
+//
+//  Created by lai on 15/7/8.
+//  Copyright (c) 2015年 laiweihuang. All rights reserved.
+//
+
+#import "BaseFrameModel.h"
+
+@interface StatusDetailFrameModel : BaseFrameModel
+
+@end
