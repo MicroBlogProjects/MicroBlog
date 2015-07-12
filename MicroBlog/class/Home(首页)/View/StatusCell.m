10a22881a5dd32973cfae269ed052e082ab06b27
赖伟煌 <laiweihuang126@qq.com>
2015-07-12 11:03:09 +0800

    1、微博首页中，点击更多按钮出现ActionSheet( 收藏，关注，举报等)。 由于API接口限制，只实现了收藏和取消收藏功能

--- MicroBlog/class/Home(首页)/View/StatusCell.m
+++ MicroBlog/class/Home(首页)/View/StatusCell.m
@@ -11,18 +11,22 @@
 #import "UserModel.h"
 #import "StatusFrameModel.h"
 #import "UIImageView+WebCache.h"
+#import "AccountTool.h"
 #import "PhotoModel.h"
 #import "ToolBar.h"
 #import "StatusPhotosView.h"
 #import "IconView.h"
 #import "NavigationController.h"
-
+#import "ZZActionSheet.h"
 #import "MainTabbarViewController.h"
 #import "NavigationController.h"
 #import "StatusDetailViewController.h"
-@interface StatusCell ()
+#import "AFNetworking.h"
+#import "MBProgressHUD+MJ.h"
+@interface StatusCell ()  <ZZActionSheetDelegate>
 
 @property (nonatomic , strong) ToolBar *toolbar;
+
 
 @end
 @implementation StatusCell
@@ -46,6 +50,7 @@
 }
 
 
+/** 点击转发微博，页面跳转到转发微博的详情页 */
 -(void)retweetClick{
     NavigationController *nav = (NavigationController *)[MainTabbarViewController sharedMainTabbarViewController].selectedViewController;
     StatusDetailViewController *detail = [[StatusDetailViewController alloc]init ] ;
@@ -55,10 +60,7 @@
 
 
 
-
-
-
-
+#pragma mark- 点击更多按钮
 
 /** 与baseCell不一样东西在这里设置*/
 -(void)addOtherObject{
@@ -71,12 +73,124 @@
     UIButton *more = [UIButton buttonWithType:UIButtonTypeCustom];
     [more setImage:[UIImage imageNamed:@"timeline_icon_more"] forState:UIControlStateNormal];
     [more setImage:[UIImage imageNamed:@"timeline_icon_more_highlighted"] forState:UIControlStateHighlighted];
+    [more addTarget:self action:@selector(clickMore) forControlEvents:UIControlEventTouchDown];
     more.frame  = CGRectMake(btnX, btnY, btnWidth, btnHeight);
     [self.contentView addSubview:more];
     
+    
+}
+
+/** 点击微博右上角的下标按钮触发 */
+-(void)clickMore{
+    
+    NSString *collectSting ;
+    if(self.baseFrameModel.statusModel.favorited){
+        collectSting = @"取消收藏";
+    }else{
+        collectSting = @"收藏";
+    }
+    
+    ZZActionSheet *actionSheet = [[ZZActionSheet alloc]initWithTitle:nil delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@[collectSting,@"帮上头条",@"取消关注",@"屏蔽",@"举报"]];
+    
+    [actionSheet showInView:[UIApplication sharedApplication].keyWindow ];
+}
+
+/**  ActionSheet 点击事件 */
+-(void)ZZActionSheet:(ZZActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    switch (buttonIndex) {
+        case 0:
+            [self collection];
+            break;
+        case 1:
+            [self showMessage:@"API接口限制无法实现"];
+            break;
+        case 2:
+            [self showMessage:@"API接口限制无法实现"];
+            break;
+        case 3:
+            [self showMessage:@"API接口限制无法实现"];
+            break;
+        case 4:
+            [self showMessage:@"API接口限制无法实现"];
+            break;
+            
+            
+        default:
+            break;
+    }
+    
+}
+/** 收藏 */
+-(void)collection{
+    
+
+    if(!self.baseFrameModel.statusModel.favorited){  //还未收藏，现在要收藏
+        
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    AccountModel *account  = [AccountTool account];
+    NSMutableDictionary *params= [NSMutableDictionary dictionary];
+    params[@"access_token"] = account.access_token ;
+    params[@"id"] = self.baseFrameModel.statusModel.idstr;
+    [manager POST:@"https://api.weibo.com/2/favorites/create.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"%@",responseObject);
+        
+        [MBProgressHUD showSuccess:@"收藏成功" toView:[UIApplication sharedApplication].keyWindow ];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUD];
+        });
+        //修改收藏状态
+        self.baseFrameModel.statusModel.favorited = !self.baseFrameModel.statusModel.favorited ;
+        
+        
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@",error);
+        [MBProgressHUD showError:@"收藏失败" toView:[UIApplication sharedApplication].keyWindow ];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUD];
+        });
+    }];
+    }else{ /* 已经收藏，现在要取消收藏 */
+        
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        AccountModel *account  = [AccountTool account];
+        NSMutableDictionary *params= [NSMutableDictionary dictionary];
+        params[@"access_token"] = account.access_token ;
+        params[@"id"] = self.baseFrameModel.statusModel.idstr;
+        [manager POST:@"https://api.weibo.com/2/favorites/destroy.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"%@",responseObject);
+            
+            [MBProgressHUD showSuccess:@"删除收藏成功" toView:[UIApplication sharedApplication].keyWindow ];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [MBProgressHUD hideHUD];
+            });
+            //修改收藏状态
+            self.baseFrameModel.statusModel.favorited = !self.baseFrameModel.statusModel.favorited ;
+            
+            
+            
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"%@",error);
+            [MBProgressHUD showError:@"删除收藏失败" toView:[UIApplication sharedApplication].keyWindow ];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [MBProgressHUD hideHUD];
+            });
+        }];
+    }
+    
+}
+
+-(void)showMessage:(NSString *)message{
+    [MBProgressHUD showError:message toView:[UIApplication sharedApplication].keyWindow ];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [MBProgressHUD hideHUDForView:[UIApplication sharedApplication].keyWindow];
+    });
 }
 
 
+#pragma mark  高亮设置
 
 
 /**  覆盖高亮显示方法  */
@@ -107,6 +221,7 @@
         [self unHighlightSubviews:self.contentView];
     }
 }
+
 
 
 /**
@@ -142,6 +257,7 @@
 //    }
 //    
 //}
+
 
 
 @end
